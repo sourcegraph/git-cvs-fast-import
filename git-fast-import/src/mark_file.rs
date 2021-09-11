a5d6876939929ff4e23fbce29f706862ef58bda6
Adam Harvey <adam@adamharvey.name>
2021-09-10 20:56:02 -0700

    Add missing file from the last commit.

---
+++ git-fast-import/src/mark_file.rs
@@ -0,0 +1,89 @@
+use std::{
+    io::{self, BufReader, Read, Seek},
+    num::ParseIntError,
+    str::FromStr,
+};
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::{alphanumeric1, digit1, multispace1},
+    combinator::map_res,
+    sequence::{delimited, terminated},
+    Finish, IResult,
+};
+use rev_lines::RevLines;
+use thiserror::Error;
+
+use crate::Mark;
+
+#[derive(Error, Debug)]
+pub(crate) enum Error {
+    #[error(transparent)]
+    IoError(#[from] io::Error),
+
+    #[error("mark parsing error: {0:?}")]
+    MarkParsingError(nom::error::ErrorKind),
+}
+
+pub(crate) fn get_last_mark<R>(reader: R) -> Result<Option<Mark>, Error>
+where
+    R: Read + Seek,
+{
+    for line in RevLines::new(BufReader::new(reader))?.into_iter() {
+        if line.len() > 0 {
+            return Ok(Some(
+                Finish::finish(mark_line(&line))
+                    .map_err(|e| Error::MarkParsingError(e.code))?
+                    .1,
+            ));
+        }
+    }
+
+    Ok(None)
+}
+
+fn mark_line(input: &str) -> IResult<&str, Mark> {
+    map_res(
+        terminated(delimited(tag(":"), digit1, multispace1), alphanumeric1),
+        |raw| -> Result<Mark, ParseIntError> { Ok(Mark::from_str(raw)?) },
+    )(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+
+    use super::*;
+
+    macro_rules! assert_get_last_mark_error {
+        ($input:expr) => {
+            assert!(get_last_mark(Cursor::new($input)).is_err());
+        };
+    }
+
+    macro_rules! assert_get_last_mark_ok {
+        ($input:expr, $want:expr) => {
+            assert_eq!(get_last_mark(Cursor::new($input)).unwrap(), $want);
+        };
+    }
+
+    #[test]
+    fn test_get_last_mark() {
+        assert_get_last_mark_ok!(b"", None);
+        assert_get_last_mark_ok!(b"\n", None);
+        assert_get_last_mark_ok!(
+            b":25 0123456789012345678901234567890123456789",
+            Some(Mark(25))
+        );
+        assert_get_last_mark_ok!(
+            b":25 0123456789012345678901234567890123456789\n\n",
+            Some(Mark(25))
+        );
+
+        assert_get_last_mark_error!(b"not a mark");
+        assert_get_last_mark_error!(b":xx xx");
+        assert_get_last_mark_error!(b":25");
+        assert_get_last_mark_error!(b":25 \n");
+        assert_get_last_mark_error!(b"25 xx");
+    }
+}
